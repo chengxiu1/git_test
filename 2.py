4ff53f3739d1ba46d8eef6a02a01c29ae03f4f88
chengxiu1 <2901266280@qq.com>
2024-10-16 01:44:02 +0800

    第二次提交

--- 2.py
+++ 2.py
@@ -10,20 +10,14 @@ class denlu(unittest.TestCase):
     def tearDown(self) -> None:
         self.driver.close()
     def test_denglu01(self):
-        self.driver.get('192.168.46.5:16097')
+        self.driver.get('http://192.168.46.5:13077')
         self.driver.find_element(By.NAME, 'username').send_keys('XTGLY')
         self.driver.find_elements(By.CLASS_NAME, 'password')[0].send_keys('123456')
         self.driver.find_element(By.XPATH, '//*[@id="signIn"]').click()
     def test_denglu02(self):
-        self.driver.get('192.168.46.5:16097')
+        self.driver.get('http://192.168.46.5:13077')
         self.driver.find_element(By.ID, 'username').send_keys('XTGLY')
         self.driver.find_element(By.CLASS_NAME, 'password').send_keys('123456')
         self.driver.find_element(By.CLASS_NAME, 'primary ').click()
-        self.driver.find_element(By.LINK_TEXT, '供应商信息').click()
-        self.driver.find_element(By.XPATH, '//*[@id="app"]/div/div[2]/section/div/div[3]/div[3]/table/tbody/tr[1]/td[6]/div/button[2]').click()
-        all = self.driver.window_handles
-        self.driver.switch_to.window(all[-1])
-        self.driver.find_element(By.XPATH, '').click()
-        self.driver.get_screenshot_as_file('test01.png')
 if __name__ == '__main__':
     unittest.main()
