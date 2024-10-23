0a413ff4e284293c1553338aa0ceedd30cb3d872
chengxiu1 <152831857+chengxiu1@users.noreply.github.com>
2024-10-23 17:11:09 +0800

    Add files via upload

--- 2.py
+++ 2.py
@@ -30,7 +30,7 @@ def test_denglu02():
     driver.find_element(By.CLASS_NAME, 'password').send_keys('123456')
     driver.find_element(By.TAG_NAME, 'button').click()
     sleep(2)
-    driver.find_element(By.LINK_TEXT, '供应商信息1').click()
+    driver.find_element(By.LINK_TEXT, '供应商信息').click()
     sleep(2)
     driver.find_element(By.XPATH,
                         '//*[@id="app"]/div/div[2]/section/div/div[3]/div[3]/table/tbody/tr/td[6]/div/button[1]/span/a').click()
@@ -44,15 +44,23 @@ def test_denglu02():
     driver.quit()
 
 
-def test_denglu03():
+def test_denglu02():
     driver = webdriver.Chrome()
     driver.implicitly_wait(3)
     driver.maximize_window()
-    driver.get('http://192.168.46.5:13077/')
+
+    driver.get('http://192.168.46.5:13077/login?redirect=%2Findex')
     driver.find_element(By.ID, 'username').send_keys('XTGLY')
     driver.find_element(By.CLASS_NAME, 'password').send_keys('123456')
     driver.find_element(By.TAG_NAME, 'button').click()
+    sleep(2)
     driver.find_element(By.LINK_TEXT, '供应商信息').click()
+    sleep(2)
+    driver.find_element(By.XPATH,
+                        '//*[@id="app"]/div/div[2]/section/div/div[3]/div[3]/table/tbody/tr/td[6]/div/button[1]/span/a').click()
+    driver.find_element(By.XPATH, '/html/body/div[2]/div/div[1]/button/i').click()
+    driver.get_screenshot_as_file('2.png')
+
     driver.quit()
 if __name__ == '__main__':
     pytest.main()
