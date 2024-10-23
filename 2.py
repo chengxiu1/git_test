9a45c7e8be65387d42b564e691bd5520ca041209
chengxiu1 <152831857+chengxiu1@users.noreply.github.com>
2024-10-23 16:59:17 +0800

    Add files via upload

--- 2.py
+++ 2.py
@@ -50,7 +50,7 @@ def test_denglu03():
     driver.maximize_window()
 
     driver.get('http://192.168.46.5:13077/login?redirect=%2Findex')
-    driver.find_element(By.ID, 'username').send_keys('XTGLY')
+    driver.find_element(By.ID, 'username1').send_keys('XTGLY')
     driver.find_element(By.CLASS_NAME, 'password').send_keys('123456')
     driver.find_element(By.TAG_NAME, 'button').click()
     sleep(2)
