14451b68971cb5e41fbd756acbc1d6bae6926eb4
chengxiu1 <152831857+chengxiu1@users.noreply.github.com>
2024-10-23 19:13:14 +0800

    Update testadd.js

--- testadd.js
+++ testadd.js
@@ -30,11 +30,8 @@ export const options = {
             executor: 'ramping-vus', 
             startVUs: 10, 
             stages: [ 
-                { duration: '1m', target: 20 }, 
-                { duration: '2m', target: 50 }, 
-                { duration: '3m', target: 100 }, 
-                { duration: '2m', target: 50 }, 
-                { duration: '1m', target: 0 }, 
+                { duration: '1m', target: 20 },  
+                { duration: '3m', target: 0 }, 
             ],
             gracefulRampDown: '30s', 
         },
