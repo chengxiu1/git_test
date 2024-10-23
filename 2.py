e52f5391cbf65ec6a26f544e495da66597632600
chengxiu1 <152831857+chengxiu1@users.noreply.github.com>
2024-10-23 16:31:07 +0800

    Add files via upload

--- 2.py
+++ 2.py
@@ -30,6 +30,21 @@ class denlu(unittest.TestCase):
         self.driver.find_element(By.XPATH, '/html/body/div[2]/div/div[1]/button/i').click()
         self.driver.get_screenshot_as_file('2.png')
 
-   
+    def test_denglu03(self):
+        self.driver.get('http://192.168.46.5:13077/login?redirect=%2Findex')
+        self.driver.find_element(By.ID, 'username').send_keys('XTGLY')
+        self.driver.find_element(By.CLASS_NAME, 'password').send_keys('123456')
+        self.driver.find_element(By.TAG_NAME, 'button').click()
+        sleep(2)
+        self.driver.find_element(By.LINK_TEXT, '供应商信息').click()
+        sleep(2)
+        self.driver.find_element(By.XPATH,
+                                 '//*[@id="app"]/div/div[2]/section/div/div[3]/div[3]/table/tbody/tr/td[6]/div/button[1]/span/a').click()
+        sleep(2)
+        all = self.driver.window_handles
+        self.driver.switch_to.window(all[-1])
+        sleep(2)
+        self.driver.find_element(By.XPATH, '/html/body/div[2]/div/div[1]/button/i/a').click()
+        self.driver.get_screenshot_as_file('2.png')
 if __name__ == '__main__':
     unittest.main()
